6018865cf2efc56fafb04ccaea4fb12dde2ba597
anasouardini <ouardinianas@proton.me>
2024-02-21 20:43:09 +0100

    refactor: make the home.tsx view more modular.

--- src/root/ui/src/home.tsx
+++ src/root/ui/src/home.tsx
@@ -11,6 +11,8 @@ import {
 } from "./viewUtils/common";
 import { ProjectsControllers } from "../../../projects/projectsControllers";
 
+declare const fakeRoot: ProjectsControllers;
+
 const myNode = document.body;
 // clear default DOM
 while (myNode.firstChild) {
@@ -33,107 +35,13 @@ const style = {
 interface Button {
   txt: string;
   data: any;
-  action: (root: ProjectsControllers) => (args: any) => any;
+  action: (root: ProjectsControllers) => (...args: any[]) => any;
+  // actionCall: (root: ProjectsControllers) => any;
 }
 // controls buttons
 const projects: Record<keyof ProjectsControllers, Button[]> = {
   home: [],
-  dreamjob: [
-    {
-      txt: "collect companies",
-      action: (root) => root.dreamjob.company.collect,
-      data: {},
-    },
-    {
-      txt: "get companies metadata",
-      action: (root) => root.dreamjob.company.getMetaData,
-      data: {},
-    },
-    {
-      txt: "collect decision makers",
-      action: (root) => root.dreamjob.company.collectDecisionMakers.start,
-      data: {},
-    },
-    {
-      txt: "connect with decision makers",
-      action: (root) => root.dreamjob.connection.start,
-      data: {},
-    },
-    //? maybe add those old connection request to a list if they don't exist in the employees table
-    // {
-    //   txt: "clean old pending connections",
-    //   action: (root)=>root.dreamjob.connection.clean,
-    //   data: {},
-    // }, // not implemented
-    // {
-    //   txt: "check connections acceptance",
-    //   action: (root)=>root.dreamjob.connection.check,
-    //   data: {},
-    // }, // not implemented
-    {
-      txt: "job requests",
-      action: (root) => root.dreamjob.company.sendJobRequests.start,
-      data: {},
-    },
-  ],
-  jobboardsMarocAnnonces: [
-    {
-      txt: "collect",
-      action: (root) => root.jobboards.marocannonces.collect,
-      data: {},
-    },
-    {
-      txt: "parse",
-      action: (root) => root.jobboards.marocannonces.parse,
-      data: {},
-    },
-    {
-      txt: "find matches",
-      action: (root) => root.jobboards.marocannonces.findMatches,
-      data: {},
-    },
-    {
-      txt: "apply",
-      action: (root) => root.jobboards.marocannonces.apply,
-      data: {},
-    },
-  ],
-  jobboardsIndeed: [
-    {
-      txt: "collect",
-      action: (root) => root.jobboards.indeed.collect,
-      data: {},
-    },
-    {
-      txt: "find matches",
-      action: (root) => root.jobboards.indeed.findMatches,
-      data: {},
-    },
-    {
-      txt: "apply",
-      action: (root) => root.jobboards.indeed.apply,
-      data: {},
-    },
-  ],
-  frugalads: [
-    {
-      txt: "frugalads-chatiwus",
-      action: (root) => root.frugalads.chatiwus.lunchInstance,
-      data: {},
-    },
-  ],
-  socialboost: [
-    {
-      txt: "reddit hunt",
-      action: (root) => root.socialboost.reddit.hunt,
-      data: {},
-    },
-    {
-      txt: "twiter hunt",
-      action: (root) => root.socialboost.twitter.hunt,
-      data: {},
-    },
-  ],
+  // TODO: get object passed to home controller
 };
 
 function Component() {
@@ -181,7 +89,9 @@ function Component() {
               {buttons.map(({ txt, action, data }) => {
                 return (
                   <button
-                    onClick={() => runServerAction({ action, data })}
+                    onClick={() => {
+                      runServerAction({ action, data });
+                    }}
                     style={{
                       ...style.btn,
                       margin: 0,
