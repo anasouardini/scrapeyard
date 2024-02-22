5636cee74d1344d5bcb4b3ddc356b246dcbe1533
anasouardini <ouardinianas@proton.me>
2024-02-22 02:51:06 +0100

    fix: get it to run (first run), install necessary tools, refactor around

--- data/templates/default/src/index.ts
+++ data/templates/default/src/index.ts
@@ -7,38 +7,42 @@ import {
 } from "scrapeyard";
 import chatgpt from "./projects/1iqai/controller/chatgpt";
 
-// initializes browser instances/windows
-await browser.init({
-  instances: [
-    // stateful: uses specified profile path
-    { stateful: true, headless: false },
-    // { stateful: false, headless: true },
-  ],
-});
-
-// runs a controller from "root.home" and passes it empty object "{}"
-await dispatcher(serverVars.drivers[0], {
-  action: ((root: ProjectsControllers) => root.home).toString(),
-  // actions that would be showed in the home view after execution
-  data: {
-    oneiqai: [
-      {
-        txt: "Apple Cake Recipe",
-        action: (root) => root.oneiqai.chatgpt.promptTemplate,
-        data: { name: "cakeRecipe", args: { type: "apple" } },
-      },
+async function main() {
+  // initializes browser instances/windows
+  await browser.init({
+    instances: [
+      // stateful: uses specified profile path
+      { stateful: true, headless: false },
+      // { stateful: false, headless: true },
     ],
-  },
-  type: "direct",
-  // direct: directly from the server (this file)
-  // scrapeyardEvent: from a view in the browser
-});
+  });
 
-// simple example of using a project.
-// "oneiqai" is a default project example at "./projects/oneiqai"
-const prompt = "what's the most popular sport in the world?";
-const response = await chatgpt.prompt(serverVars.drivers[0], prompt);
-console.log({
-  prompt,
-  response,
-});
+  // runs a controller from "root.home" and passes it empty object "{}"
+  await dispatcher(serverVars.windows[0], {
+    action: ((root: ProjectsControllers) => root.home).toString(),
+    // actions that would be showed in the home view after execution
+    data: {
+      oneiqai: [
+        {
+          txt: "Apple Cake Recipe",
+          action: (root) => root.oneiqai.chatgpt.promptTemplate,
+          data: { name: "cakeRecipe", args: { type: "apple" } },
+        },
+      ],
+    },
+    type: "direct",
+    // direct: directly from the server (this file)
+    // scrapeyardEvent: from a view in the browser
+  });
+
+  // simple example of using a project.
+  // "oneiqai" is a default project example at "./projects/oneiqai"
+  const prompt = "what's the most popular sport in the world?";
+  const response = await chatgpt.prompt(serverVars.windows[0], prompt);
+  console.log({
+    prompt,
+    response,
+  });
+}
+
+main();
