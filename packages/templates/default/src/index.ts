71217c1cc5154a6330e616fdd3194d577725f571
anasouardini <ouardinianas@proton.me>
2024-02-27 20:10:23 +0100

    feat: use the new feature for lunching the scraper

--- packages/templates/default/src/index.ts
+++ packages/templates/default/src/index.ts
@@ -5,7 +5,6 @@ import {
   type ProjectsControllers,
   type Actions,
 } from 'scrapeyard';
-import util from 'util';
 
 const actions: Partial<Actions> = {
   frugalads: [
@@ -28,13 +27,13 @@ const config = {
     ],
   },
   dispatch: {
-    window: serverVars.windows[0],
+    windowIndex: 0,
     // runs a controller from "root.home" and passes it empty object "{}"
-    action: {
-      action: ((root: ProjectsControllers) => root.home.load).toString(),
+    msg: {
+      action: (root: { home: { load: () => void } }) => root.home.load,
       // actions/button that would be showed in the home view
       data: actions,
-      type: 'direct',
+      type: 'direct' as 'direct' | 'scrapeyardEvent',
       // direct: directly from the server (this file)
       // scrapeyardEvent: from a view in the browser
     },
