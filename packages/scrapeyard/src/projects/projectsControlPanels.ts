8691db77adfbf582d0d9f8a375480efb8f77c2db
anasouardini <ouardinianas@proton.me>
2024-02-27 19:58:25 +0100

    fix: no need for the indexer files when the code-gen is working 100% now

--- packages/scrapeyard/src/projects/projectsControlPanels.ts
+++
@@ -1,8 +0,0 @@
-// [import above this line]
-
-const controlPanels = {
-  // [add to object above this line]
-};
-
-export type ProjectName = keyof typeof controlPanels;
-export default controlPanels;
