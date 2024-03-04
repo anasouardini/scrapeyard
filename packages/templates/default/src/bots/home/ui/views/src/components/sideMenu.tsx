6eb002185a76c01ffc636b410be47f3d88677ae3
anasouardini <ouardinianas@proton.me>
2024-03-04 21:49:29 +0100

    feat: use generated actionsList inside the side menu of the home interface

--- packages/templates/default/src/bots/home/ui/views/src/components/sideMenu.tsx
+++ packages/templates/default/src/bots/home/ui/views/src/components/sideMenu.tsx
@@ -1,65 +1,9 @@
 import React from 'react';
-
-const controllers = {
-  jobboards: {
-    action1: () => {
-      console.log('action1 original');
-    },
-    obj1: {
-      action12: () => {
-        console.log('action12');
-      },
-    },
-    obj2: {
-      action13: () => {
-        console.log('action13');
-      },
-    },
-  },
-};
-
-function extractBotActions(bot: Record<string, any>) {
-  const actions: { name: string; action: () => any }[] = [];
-  function recursiveExtractor(
-    parent: (() => any) | Record<string, any>,
-    parentName: string,
-    actionName: string,
-  ) {
-    const supposedlyAction = parent[actionName] as () =>
-      | any
-      | Record<string, any>;
-
-    // if it really an action
-    if (typeof supposedlyAction === 'function') {
-      const isActionNameTaken = Object.values(actions).some((action) => {
-        return action.name === actionName;
-      });
-      if (isActionNameTaken) {
-        actions.push({
-          name: `${parentName} - ${actionName}`,
-          action: supposedlyAction,
-        });
-        return;
-      }
-      actions.push({ name: actionName, action: supposedlyAction });
-    } else if (typeof supposedlyAction == 'object') {
-      const supposedlyActions = Object.entries(
-        supposedlyAction as Record<string, any>,
-      );
-      for (const hopefullyAction of supposedlyActions) {
-        recursiveExtractor(supposedlyAction, actionName, hopefullyAction[0]);
-      }
-    }
-  }
-  recursiveExtractor({ bot }, 'BOT', 'bot');
-
-  return actions;
-}
+import botsActions from '$/../botsActions';
 
 function SideMenu() {
   function listActions(botName: string) {
-    //* recursive function
-    const actions = extractBotActions(controllers[botName]);
+    const actions = botsActions;
     return (
       <ul>
         {actions.map((action) => {
