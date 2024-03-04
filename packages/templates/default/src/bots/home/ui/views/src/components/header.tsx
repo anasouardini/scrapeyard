e3df5fb6dbbf444e037090c9898fd16c0cd24bca
anasouardini <ouardinianas@proton.me>
2024-03-04 04:13:02 +0100

    fix: header.tsx can use yup now after vite bug was fixed

--- packages/templates/default/src/bots/home/ui/views/src/components/header.tsx
+++ packages/templates/default/src/bots/home/ui/views/src/components/header.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-// import yup from 'yup';
+import * as yup from 'yup';
 import CustomForm from './customForm';
 
 interface Props {}
 function Header(props: Props) {
-  //   const schema = yup.object().shape({ name: yup.string().required().max(100) });
+  const schema = yup.object().shape({ name: yup.string().required().max(100) });
   const refs = React.useRef({
     fields: { name: '' },
   });
 
   return (
-    <header className='flex gap-4'>
+    <header className='flex flex-col gap-4 w-full'>
       <button>Home</button>
-      <CustomForm fields={refs.current.fields} schema={{}} />
+      <CustomForm fields={refs.current.fields} schema={schema} />
     </header>
   );
 }
