96337de3d3d008302982f47d5423ba026dc6a628
anasouardini <ouardinianas@proton.me>
2024-03-05 03:44:02 +0100

    fix: fixed a typo in botsUtils name

--- packages/scrapeyard/scripts/botsActionsGen.ts
+++ packages/scrapeyard/scripts/botsActionsGen.ts
@@ -5,7 +5,7 @@ import path from 'path';
 import fs from 'fs';
 
 const vars = {
-  botsUtilsName: 'botsUtis',
+  botsUtilsName: 'botsUtils',
 };
 
 const config = {
--- packages/templates/default/botsUtils.ts
+++ packages/templates/default/botsUtils.ts
@@ -1,14 +1,52 @@
-import home from './src/bots/home/controller';
-import indeed from './src/bots/jobboards/controller/indeed';
-import marocannonces from './src/bots/jobboards/controller/marocannonces';
-
-export const botsActions = { home: home, jobboards: { indeed, marocannonces } };
-export type ProjectsControllers = typeof botsActions;
-export type BotsControllers = typeof botsActions;
-
-export interface HomeButton {
-  txt: string;
-  action: (root: BotsControllers) => (...args: any[]) => any;
-  data: {};
-}
-export type HomeButtons = Record<keyof BotsControllers, HomeButton[]>;
+export const botsActions = [
+  { name: 'load', action: (root) => root.home.load },
+  {
+    name: 'collect',
+    action: (root) => root.jobboards.indeed.collect,
+  },
+  {
+    name: 'apply',
+    action: (root) => root.jobboards.indeed.apply,
+  },
+  {
+    name: 'findMatches',
+    action: (root) => root.jobboards.indeed.findMatches,
+  },
+  {
+    name: 'getValidPosts',
+    action: (root) => root.jobboards.indeed.getValidPosts,
+  },
+  {
+    name: 'setMatchingAndUnmatchingPosts',
+    action: (root) => root.jobboards.indeed.setMatchingAndUnmatchingPosts,
+  },
+  {
+    name: 'updatePostStatus',
+    action: (root) => root.jobboards.indeed.updatePostStatus,
+  },
+  {
+    name: 'marocannonces - collect',
+    action: (root) => root.jobboards.marocannonces.collect,
+  },
+  {
+    name: 'parse',
+    action: (root) => root.jobboards.marocannonces.parse,
+  },
+  {
+    name: 'marocannonces - apply',
+    action: (root) => root.jobboards.marocannonces.apply,
+  },
+  {
+    name: 'marocannonces - findMatches',
+    action: (root) => root.jobboards.marocannonces.findMatches,
+  },
+  {
+    name: 'marocannonces - getValidPosts',
+    action: (root) => root.jobboards.marocannonces.getValidPosts,
+  },
+  {
+    name: 'marocannonces - setMatchingAndUnmatchingPosts',
+    action: (root) =>
+      root.jobboards.marocannonces.setMatchingAndUnmatchingPosts,
+  },
+];
