9a7e40d2a34265ac6dc96f277b6db0a1b6f6b906
anasouardini <ouardinianas@proton.me>
2024-03-07 17:56:24 +0100

    feat: added links to the shared layout

--- packages/templates/default/src/bots/home/ui/views/src/components/sharedLayout.tsx
+++ packages/templates/default/src/bots/home/ui/views/src/components/sharedLayout.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
 import * as yup from 'yup';
 import CustomForm from './customForm';
+import { Outlet } from 'react-router';
+import { NavLink } from 'react-router-dom';
 
-interface Props {
-  children: string | JSX.Element | JSX.Element[];
-}
+interface Props {}
 function Header(props: Props) {
   const schema = yup.object().shape({ name: yup.string().required().max(100) });
   const refs = React.useRef({
     fields: { name: '' },
   });
 
+  const style = {
+    header: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: '10px 2rem',
+      background: 'var(--clr-bg3)',
+    },
+    title: {
+      fontSize: '1.5rem',
+      fontWeight: 'bold',
+    },
+    navItem: {
+      fontWeight: 'bold',
+      fontSize: '1.2rem',
+    },
+  };
+
   return (
     <>
-      <header className='flex flex-col gap-4 w-full'>
-        <button>Home</button>
-        <CustomForm fields={refs.current.fields} schema={schema} />
+      <header style={style.header}>
+        <NavLink
+          to='/'
+          style={style.navItem}
+          className={({ isActive }) => (isActive ? 'activeNavLink' : '')}
+        >
+          Projects
+        </NavLink>
+        {/* <CustomForm fields={refs.current.fields} schema={schema} /> */}
       </header>
-      <main>{props.children}</main>
+      <main style={{ paddingTop: '2rem', background: 'var(--clr-bg2)' }}>
+        <Outlet />
+      </main>
     </>
   );
 }
