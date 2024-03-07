c425d80214860a9fa5aa67c80d661a01f6d704cc
anasouardini <ouardinianas@proton.me>
2024-03-07 17:56:45 +0100

    feat: project page is done

--- packages/templates/default/src/bots/home/ui/views/src/pages/project.tsx
+++ packages/templates/default/src/bots/home/ui/views/src/pages/project.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import SideMenu from '$home-components/sideMenu';
 import BotControlPanel from '$home-components/botControlPanel';
+import { useParams } from 'react-router';
 
 interface Props {}
 export default function Project(props: Props) {
+  const { name } = useParams<{ name: 'jobboards' | 'home' }>();
+  if (!name) {
+    return <div>Please provide a valid project name</div>;
+  }
+  const style = {
+    container: {
+      display: 'flex',
+      paddingInline: '2rem',
+    },
+  };
+
   return (
-    <>
-      <SideMenu />
-      <BotControlPanel name='jobboards' />
-    </>
+    <div style={style.container}>
+      <BotControlPanel name={name} />
+      <SideMenu projectName={name} />
+    </div>
   );
 }
